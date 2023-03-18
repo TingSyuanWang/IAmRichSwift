72b63aa7664c1d057d84cf90af434dcd50c1d0d3
TingSyuanWang <vincent@fishboneapps.com>
2023-03-18 16:36:26 -0400

    Initial Commit

---
+++ IAmRichSwift/IAmRichSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRichSwiftApp.swift
+//  IAmRichSwift
+//
+//  Created by Ting-Hsuan Wang on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct IAmRichSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
